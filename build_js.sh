e9afd95afdcdc00332deae6f2704cbc0c88ba7c0
mz <emgyrz@gmail.com>
2019-06-14 01:07:38 +0300

    sync versions

--- build_js.sh
+++ build_js.sh
@@ -3,11 +3,18 @@
 # Windows
 cross build --release --target x86_64-pc-windows-gnu && \
 cp ./target/x86_64-pc-windows-gnu/release/tagger.exe ./mjs/tagger-win64/ && \
+echo "Binary for Windows (x64) created" && \
 
 # Linux
 cargo build --release && \
-cp ./target/release/tagger ./mjs/tagger-linux64/
-
+cp ./target/release/tagger ./mjs/tagger-linux64/ && \
+echo "Binary for Linux (x64) created" && \
 
 # OSX
 # TODO
+
+
+# README
+cp README.md ./mjs/ && \
+echo "README file copied"
+
---
+++ mjs/README.md
@@ -0,0 +1,3 @@
+# tagger
+
+Simple utility to do something (e.g. install) packages from git repository by selecting git tag which is presented by semver rules
