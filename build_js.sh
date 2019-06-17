46329685840c61228b8f696e6b6e5736318674b3
mz <emgyrz@gmail.com>
2019-06-18 01:41:22 +0300

    build.sh fix

--- build_js.sh
+++ build_js.sh
@@ -2,11 +2,13 @@
 
 # Windows
 cross build --release --target x86_64-pc-windows-gnu && \
+mkdir -p ./mjs/tagger-win64 && \
 cp ./target/x86_64-pc-windows-gnu/release/tagger.exe ./mjs/tagger-win64/ && \
 echo "Binary for Windows (x64) created" && \
 
 # Linux
 cargo build --release && \
+mkdir -p ./mjs/tagger-linux64 && \
 cp ./target/release/tagger ./mjs/tagger-linux64/ && \
 echo "Binary for Linux (x64) created" && \
 
